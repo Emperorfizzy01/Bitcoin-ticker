21e7e1f3f72f5a8cdb32fd5510a5b9574a5053e1
Emperorfizzy01 <farukgbolahan21@gmail.com>
2022-03-29 20:18:13 +0100

    First commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+var url = 'https://rest.coinapi.io/v1/exchangerate';
+var apiKey = '2E32F52F-4253-4057-A7DC-5A7CBE991F98';
+
+class CoinData {
+  Future getCryptoPrice(String selectedCurrency) async {
+    http.Response response = await http.get(
+      Uri.parse('$url/BTC/$selectedCurrency?apiKey=$apiKey'),
+    );
+    if (response.statusCode == 200) {
+      String body = response.body;
+      var decodeData = jsonDecode(body);
+      double lastPrice = decodeData['rate'];
+      return lastPrice.toStringAsFixed(0);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
+import 'package:bitcoin_ticker/price_screen.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,81 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  var bitcoinValueInUSD = '?';
+
+  CoinData coin = CoinData();
+
+  void getData() async {
+    try {
+      var data = await CoinData().getCryptoPrice(selectedCurrency);
+
+      setState(() {
+        bitcoinValueInUSD = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdown = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      var currency = currenciesList[i];
+      var newItem = DropdownMenuItem(child: Text(currency), value: currency);
+      dropdown.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdown,
+      onChanged: (String newValue) {
+        setState(() {
+          selectedCurrency = newValue;
+
+          getData();
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iosdropdown() {
+    List<Text> cupertino = [];
+    for (String currency in currenciesList) {
+      var newItem = Text(
+        currency,
+        style: TextStyle(color: Colors.white),
+      );
+      cupertino.add(newItem);
+    }
+
+    return CupertinoPicker(
+        itemExtent: 32.0,
+        onSelectedItemChanged: (selectedIndex) {
+          print(selectedIndex);
+          setState(() {
+            selectedCurrency = currenciesList[selectedIndex];
+
+            getData();
+          });
+        },
+        children: cupertino);
+  }
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return iosdropdown();
+    } else if (Platform.isAndroid) {
+      return androidDropdown();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +106,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +121,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: getPicker(),
           ),
         ],
       ),
